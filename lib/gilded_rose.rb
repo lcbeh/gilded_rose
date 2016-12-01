403d451c30044e2ecbfa91e05bd9d0661b61210e
lcbeh <lichien2010@yahoo.co.uk>
2016-12-01 14:13:42 +0000

    refactoring legacy code - in progress

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,63 +6,50 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+      if item.name == "Sulfuras, Hand of Ragnaros"
+        #do nothing
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
+
+
+
+        if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if item.quality > 0
+            item.quality -= 1
           end
         else
           if item.quality < 50
             item.quality = item.quality + 1
+            if item.name == "Backstage passes to a TAFKAL80ETC concert"
+              item.quality += 1 if item.sell_in < 11
+              item.quality += 1 if item.sell_in < 6
+            end
+          end
+        end
+
+
+          item.sell_in = item.sell_in - 1
+
+
+
+        if item.sell_in < 0
+          if item.name != "Aged Brie"
+            if item.name != "Backstage passes to a TAFKAL80ETC concert"
+              if item.quality > 0
+                item.quality = item.quality - 1
+              end
+            else
+              item.quality = item.quality - item.quality #backstage pass
+            end
+          else
+            if item.quality < 50
+              item.quality = item.quality + 1
+            end
           end
         end
       end
+
     end
-  end
-end
 
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
   end
 
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
-  end
 end
