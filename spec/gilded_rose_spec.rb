3d467b869fd0267b5905ebb154a5e0abf2882d53
lcbeh <lichien2010@yahoo.co.uk>
2016-12-01 11:11:55 +0000

    add test for sulfuras

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -14,22 +14,22 @@ describe GildedRose do
         expect(@items[0].name).to eq "honey"
       end
 
-      it "lowers sell_in day by one" do
+      it "lowers common items sell_in day by one" do
         @gilded_rose.update_quality()
         expect(@items[0].sell_in).to eq 4
       end
 
-      it "lowers quality value by one" do
+      it "lowers common items quality value by one" do
         @gilded_rose.update_quality()
         expect(@items[0].quality).to eq 9
       end
 
-      it "checks quality value cannot be negative" do
+      it "checks all quality value cannot be negative" do
         12.times { @gilded_rose.update_quality }
         expect(@items[0].quality).to eq 0
       end
 
-      it "drops quality value by 2 if sell by date has passed" do
+      it "drops common items quality value by 2 if sell by date has passed" do
         6.times { @gilded_rose.update_quality }
         expect(@items[0].quality).to eq 3
       end
@@ -41,14 +41,31 @@ describe GildedRose do
         @gilded_rose = GildedRose.new(@items)
       end
 
-      it "adds quality value by one" do
+      it "adds quality value of aged brie by one" do
         @gilded_rose.update_quality
         expect(@items[0].quality).to eq 41
       end
 
-      it "ensures quality value cannot be more than 50"  do
+      it "ensures all quality value cannot be more than 50"  do
         100.times { @gilded_rose.update_quality }
         expect(@items[0].quality).to eq 50
+      end
+    end
+
+    context "Sulfuras" do
+      before(:each) do
+        @items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 40)]
+        @gilded_rose = GildedRose.new(@items)
+      end
+
+      it "doesn't change Sulfuras sell_in day" do
+        @gilded_rose.update_quality
+        expect(@items[0].sell_in).to eq 10
+      end
+
+      it "doesn't change Sulfuras quality" do
+        @gilded_rose.update_quality
+        expect(@items[0].quality).to eq 40
       end
     end
 
