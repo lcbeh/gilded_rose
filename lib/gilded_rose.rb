9696a3e1311081fddd3edfdc2ba3259a7c0cd65b
lcbeh <lichien2010@yahoo.co.uk>
2016-12-01 14:18:27 +0000

    Refactor aged brie condition

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,11 +8,11 @@ class GildedRose
     @items.each do |item|
       if item.name == "Sulfuras, Hand of Ragnaros"
         #do nothing
+      elsif item.name == "Aged Brie"
+        item.quality += 1 if item.quality < 50
       else
 
-
-
-        if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.name != "Backstage passes to a TAFKAL80ETC concert"
           if item.quality > 0
             item.quality -= 1
           end
@@ -32,7 +32,6 @@ class GildedRose
 
 
         if item.sell_in < 0
-          if item.name != "Aged Brie"
             if item.name != "Backstage passes to a TAFKAL80ETC concert"
               if item.quality > 0
                 item.quality = item.quality - 1
@@ -40,11 +39,6 @@ class GildedRose
             else
               item.quality = item.quality - item.quality #backstage pass
             end
-          else
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
         end
       end
 
